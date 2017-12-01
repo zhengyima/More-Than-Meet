bd310118a97058f6f5635bf24523f6d8dc6d453a
lmerengues <317958662@qq.com>
2017-12-01 22:37:38 +0800

    第二个页面前端基本完成

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -4,12 +4,13 @@ const util = require('../../utils/util.js')
 Page({
   data: {
     imgUrls: [
+      '../../images/girl2.jpeg',
       '../../images/girl.jpeg',
       'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493698928333&di=99be91f1067ce820af8235607706813a&imgtype=0&src=http%3A%2F%2Fimg.tupianzj.com%2Fuploads%2Fallimg%2F160412%2F9-160412091538.jpg',
-      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493698928333&di=ae56672831512cc7d4cd1e26d31269aa&imgtype=0&src=http%3A%2F%2Fimg.tupianzj.com%2Fuploads%2Fallimg%2F160412%2F9-160412091540.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493698928333&di=ae56672831512cc7d4cd1e26d31269aa&imgtype=0&src=http%3A%2F%2Fimg.tupianzj.com%2Fuploads%2Fallimg%2F160412%2F9-160412091540.jpg'
     ],
-    indicatorDots: false,
+    indicatorDots: true,
+
     autoplay: false,
     interval: 5000,
     duration: 1000

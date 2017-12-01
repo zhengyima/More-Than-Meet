b3d3848a5a17ec0db4226093b94252d4143d6056
lmerengues <317958662@qq.com>
2017-12-02 01:01:59 +0800

    datelist初版

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,6 +5,7 @@ Page({
   data: {
     imgUrls: [
       '../../images/girl.jpeg',
+      '../../images/girl2.jpeg',
       'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493698928333&di=99be91f1067ce820af8235607706813a&imgtype=0&src=http%3A%2F%2Fimg.tupianzj.com%2Fuploads%2Fallimg%2F160412%2F9-160412091538.jpg',
       'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493698928333&di=ae56672831512cc7d4cd1e26d31269aa&imgtype=0&src=http%3A%2F%2Fimg.tupianzj.com%2Fuploads%2Fallimg%2F160412%2F9-160412091540.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
